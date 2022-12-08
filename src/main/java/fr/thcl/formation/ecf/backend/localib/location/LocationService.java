44657ff7f79101d8a4d96d6dbd1421a4eee1f592
Thib <clement.thibaut.guillaume@gmail.com>
2022-12-08 16:09:55 +0100

    feature: ajout diverses query pour Location

--- src/main/java/fr/thcl/formation/ecf/backend/localib/location/LocationService.java
+++ src/main/java/fr/thcl/formation/ecf/backend/localib/location/LocationService.java
@@ -2,6 +2,7 @@ package fr.thcl.formation.ecf.backend.localib.location;
 
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface LocationService {
@@ -15,4 +16,18 @@ public interface LocationService {
     ResponseEntity<String> deleteById(String id);
 
     ResponseEntity<String> deleteAll();
+
+    List<Location> findByLocataire(String idLocataire);
+
+    List<Location> findByLocataireName(String nom);
+
+    List<Location> findByVehicule(String idVehicule);
+
+    List<Location> findByVehiculeMarque(String marque);
+
+    List<Location> findByDateDebut(LocalDate dateDebut);
+
+    List<Location> findByDateFin(LocalDate dateFin);
+
+
 }
--- src/main/java/fr/thcl/formation/ecf/backend/localib/location/LocationServiceImpl.java
+++ src/main/java/fr/thcl/formation/ecf/backend/localib/location/LocationServiceImpl.java
@@ -1,13 +1,14 @@
 package fr.thcl.formation.ecf.backend.localib.location;
 
-import fr.thcl.formation.ecf.backend.localib.locataire.LocataireRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocationServiceImpl implements LocationService{
@@ -52,5 +53,119 @@ public class LocationServiceImpl implements LocationService{
     public ResponseEntity<String> deleteAll() {
         locatationRepository.deleteAll();
         return new ResponseEntity<>("Suppression de tous les locataires", HttpStatus.ACCEPTED);
+    }
+
+    @Override
+    public List<Location> findByLocataire(String idLocataire) {
+        List<Location> locationList = this.findAll();
+        List<Location> locationsByLocataire = new ArrayList<>();
+
+        for (Location location: locationList) {
+            if (location.getLocataire().getId().equals(idLocataire)){
+                locationsByLocataire.add(location);
+            }
+        }
+
+        if (locationsByLocataire.isEmpty()) {
+            logger.warn("Il n'existe aucune location avec ce locataire.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Il n'existe aucune location avec ce locataire.");
+        } else {
+            return locationsByLocataire;
+        }
+    }
+
+    @Override
+    public List<Location> findByLocataireName(String nom) {
+        List<Location> locationList = this.findAll();
+        List<Location> locationsByLocataireName = new ArrayList<>();
+
+        for (Location location: locationList) {
+            if (location.getLocataire().getNom().equalsIgnoreCase(nom)) {
+                locationsByLocataireName.add(location);
+            }
+        }
+
+        if (locationsByLocataireName.isEmpty()) {
+            logger.warn("Il n'existe aucune location avec ce nom de locataire.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Il n'existe aucune location avec ce nom de locataire.");
+        } else {
+            return locationsByLocataireName;
+        }
+    }
+
+    @Override
+    public List<Location> findByVehicule(String idVehicule) {
+        List<Location> locationList = this.findAll();
+        List<Location> locationsByVehicule = new ArrayList<>();
+
+        for (Location location: locationList) {
+            if (location.getVehicule().getId().equals(idVehicule)){
+                locationsByVehicule.add(location);
+            }
+        }
+
+        if (locationsByVehicule.isEmpty()) {
+            logger.warn("Il n'existe aucune location avec ce vehicule.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Il n'existe aucune location avec ce vehicule.");
+        } else {
+            return locationsByVehicule;
+        }
+    }
+
+    @Override
+    public List<Location> findByVehiculeMarque(String marque) {
+        List<Location> locationList = this.findAll();
+        List<Location> locationsByVehiculeMarque = new ArrayList<>();
+
+        for (Location location: locationList) {
+            if (location.getVehicule().getMarque().equalsIgnoreCase(marque)) {
+                locationsByVehiculeMarque.add(location);
+            }
+        }
+
+        if (locationsByVehiculeMarque.isEmpty()) {
+            logger.warn("Il n'existe aucune location avec cette marque de véhicule.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Il n'existe aucune location cette marque de véhicule.");
+        } else {
+            return locationsByVehiculeMarque;
+        }
+    }
+
+    @Override
+    public List<Location> findByDateDebut(LocalDate dateDebut) {
+        List<Location> locationList = this.findAll();
+        List<Location> locationsByDateDebut = new ArrayList<>();
+
+        for (Location location: locationList) {
+            if (location.getDateDebut().equals(dateDebut)){
+                locationsByDateDebut.add(location);
+            }
+        }
+
+        if (locationsByDateDebut.isEmpty()) {
+            logger.warn("Il n'existe aucune location a cette date de debut.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Il n'existe aucune location a cette date de debut.");
+        } else {
+            return locationsByDateDebut;
+        }
+    }
+
+    @Override
+    public List<Location> findByDateFin(LocalDate dateFin) {
+        List<Location> locationList = this.findAll();
+        List<Location> locationsByDateFin = new ArrayList<>();
+
+        for (Location location: locationList) {
+            if (location.getDateFin().equals(dateFin)){
+                locationsByDateFin.add(location);
+            }
+        }
+
+        if (locationsByDateFin.isEmpty()) {
+            logger.warn("Il n'existe aucune location a cette date de fin.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Il n'existe aucune location a cette date de fin.");
+        } else {
+            return locationsByDateFin;
+        }
     }
 }
